13fec4711697f6cb4d33069d05c67bfc9f08eb7a
Beth Kimani <kbwaceke@gmail.com>
2024-11-05 06:51:13 +0300

    Added my react_hooks

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -1,11 +1,10 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import React from "react";
-
 import Article from "../components/Article";
 
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
-
+  screen.debug();  // Add this line to see the DOM output in the console
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
